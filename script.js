7b51ccd21681084cd23231accf682817a5645fa4
Luiz Filipe <luizfilipelgs@gmail.com>
2022-06-07 18:17:58 -0300

    js requisito 3 em andamento

---
+++ script.js
@@ -0,0 +1,13 @@
+const login = document.getElementById('email');
+const senha = document.getElementById('senha');
+/* console.log(`email: ${login}`);
+console.log(`senha: ${senha}`); */
+
+function validate() {
+    event.preventDefault;
+    if (login.value === 'trybe@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
