2937d49f3588f1a3aa4a36808ceceac3df376e67
Luiz Filipe <luizfilipelgs@gmail.com>
2022-06-07 22:51:41 -0300

    finalização requisitos 3 e 18 de js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-/* const login = document.getElementById('email');
-const senha = document.getElementById('senha');
-console.log(`email: ${login}`);
-console.log(`senha: ${senha}`);
+const logar = document.getElementById('logar');
 
-function validate() {
-    event.preventDefault;
-    if (login.value === 'trybe@teste.com' && senha.value === '123456') {
+logar.addEventListener('click', (pv) => {
+  pv.preventDefault();
+  const login = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+  console.log(`email: ${login}`);
+  console.log(`senha: ${senha}`);
+  if (login === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos');
+    alert('Email ou senha inválidos.');
   }
-} */
+});
+
+const termo = document.getElementById('agreement');
+const enviar = document.getElementById('submit-btn');
+enviar.disabled = true;
+
+function termoAceito() {
+  if (termo.checked === true) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+}
+
+termo.addEventListener('change', termoAceito);
